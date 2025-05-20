5796bb7598447d1cad2eac488b8d53f534045d4e
jeojw <jeawookjeong@gmail.com>
2025-05-20 13:37:59 +0900

    behavior tree 기초 코드 추가, ml agent 설치

---
+++ Assets/Behavior Tree/INode.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public interface INode
+{
+    public enum ENodeState
+    {
+        ENS_Running,
+        ENS_Success,
+        ENS_Failure,
+    }
+
+    public ENodeState Evaluate();
+}
